a9a2117a1f8047908d12b97f5bcd90801bffc440
MosheSilverstein <moshechayimsilverstein@gmail.com>
2018-07-23 11:16:14 -0400

    Added updated Drugbank scripts

--- Drugbank/my_functions.py
+++ Drugbank/my_functions.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import scipy.stats.mstats as stat
 import scipy.spatial.distance as dist
+from statsmodels.distributions.empirical_distribution import ECDF
+from scipy import stats
+import zipfile
 
 
 def merge(inputDF, axis, method):
@@ -17,6 +20,24 @@ def merge(inputDF, axis, method):
 
 def zscore(inputDF, axis):
     if axis == 'row':
+        inputDF.reset_index(inplace=True)
+        genes = inputDF.iloc[:, 0].values.tolist()
+        inputDF.drop(inputDF.columns[0], axis=1, inplace=True)
+        # newMatrloc = stats.zscore(inputDF, axis=0, ddof=1)
+        # newDF = pd.DataFrame(data=newMatrloc, index=inputDF.index, columns=inputDF.columns)
+
+        # for i,index in enumerate(inputDF.index):
+        #
+        #     progressPercent = ((i+1)/len(inputDF.index))*100
+        #
+        #     sys.stdout.write("Progress: %d%%  %d Out of %d   \r" % (progressPercent, (i+1), len(inputDF.index)))
+        #     sys.stdout.flush()
+        #
+        #     mean = inputDF.loc[index].mean()
+        #     std = inputDF.loc[index].std()
+        #     inputDF.loc[index] = inputDF.loc[index].apply(lambda x: ((x-mean)/std))
+
+        #Modefied Z Score
         for i,index in enumerate(inputDF.index):
 
             progressPercent = ((i+1)/len(inputDF.index))*100
@@ -24,9 +45,19 @@ def zscore(inputDF, axis):
             sys.stdout.write("Progress: %d%%  %d Out of %d   \r" % (progressPercent, (i+1), len(inputDF.index)))
             sys.stdout.flush()
 
-            mean = inputDF.ix[index].mean()
-            std = inputDF.ix[index].std()
-            inputDF.ix[index] = inputDF.ix[index].apply(lambda x: ((x-mean)/std))
+            median_y = np.median(inputDF.iloc[index])
+            median_absolute_deviation_y = np.median([np.abs(y - median_y) for y in inputDF.iloc[index]])
+            mean_absolute_deviation_y = np.mean([np.abs(y - median_y) for y in inputDF.iloc[index]])
+
+            if median_absolute_deviation_y !=0:
+                modified_z_scores = [0.6745 * (y - median_y) / median_absolute_deviation_y
+                                     for y in inputDF.iloc[index]]
+            else:
+                modified_z_scores = [(y - median_y) / ( 1.253314 * mean_absolute_deviation_y )
+                                     for y in inputDF.iloc[index]]
+            inputDF.iloc[index] = modified_z_scores
+            inputDF.index = genes
+
     elif axis == 'column':
         for i,col in enumerate(inputDF.columns):
 
@@ -39,6 +70,8 @@ def zscore(inputDF, axis):
             std = inputDF[col].std()
             inputDF[col] = inputDF[col].apply(lambda x: ((x-mean)/std))
 
+    # return(newDF)
+
 
 def log2(inputDF):
     DF = inputDF.copy()
@@ -48,6 +81,11 @@ def log2(inputDF):
 
 def quantileNormalize(inputDF):
     df = inputDF.copy()
+
+    attributes = df.columns.values.tolist()
+
+    df.columns = np.arange(0, len(attributes))
+
     #compute rank
     dic = {}
     for i,col in enumerate(df):
@@ -71,6 +109,8 @@ def quantileNormalize(inputDF):
 
         t = np.searchsorted(np.sort(df[col]), df[col])
         df[col] = [rank[i] for i in t]
+
+    df.columns = attributes
     return df
 
 mappingDF = pd.read_csv('/Users/moshesilverstein/Documents/Harmonizome/mappingFile_2017.txt', sep='\t', header=None, index_col=0)
@@ -89,8 +129,8 @@ def mapgenesymbols(inputDF):
         sys.stdout.write("Progeres: %d%%  %d Out of %d   \r" % (progressPercent, (i+1), len(inputDF.index)))
         sys.stdout.flush()
 
-        if inputDF.ix[index, inputDF.columns[0]] in mappingDF.index:
-            lst1.append(mappingDF.ix[inputDF.ix[index, inputDF.columns[0]], 1])
+        if inputDF.loc[index, inputDF.columns[0]] in mappingDF.index:
+            lst1.append(mappingDF.loc[inputDF.loc[index, inputDF.columns[0]], 1])
         else:
             lst1.append(np.nan)
 
@@ -101,31 +141,102 @@ def mapgenesymbols(inputDF):
     inputDF.dropna(inplace=True, subset=[inputDF.columns[0]])
     inputDF.set_index(inputDF.columns[0], inplace=True)
 
-def createTertiaryMarix(inputDF):
+def createTertiaryMatrix(inputDF):
+
+    # percent = 0.10
+
     df = inputDF.copy()
-    for i,col in enumerate(inputDF.columns):
 
-        progressPercent = ((i+1)/len(inputDF.columns))*100
+    # def mapter2(x):
+    #
+    #     if x > up:
+    #         return 1
+    #     elif x < down:
+    #         return -1
+    #     else:
+    #         return 0
 
-        sys.stdout.write("Progeres: %d%%  %d Out of %d   \r" % (progressPercent, (i+1), len(inputDF.columns)))
-        sys.stdout.flush()
+    # def mapter(x):
+    #
+    #     if x > up:
+    #         return 1
+    #     elif x < down:
+    #         return -1
+    #     else:
+    #         return 0
 
-        lst = []
-        lst = inputDF[col].values.tolist()
-        lst.sort()
+    # def mapter2(x):
+    #     if x >= 0.95:
+    #         return x
+    #     elif x <= -0.95:
+    #         return x
+    #     else:
+    #         return 0
 
-        lst = lst[::-1]
-        upCutoff = lst[int(0.1*len(lst))]
-        index = inputDF[inputDF[col] > upCutoff].index
-        df.ix[index, col] = 1
+    def mapter(x):
+        if x >= 0.95:
+            return 1
+        elif x <= -0.95:
+            return -1
+        else:
+            return 0
 
-        zeroIndex = inputDF[inputDF[col] < upCutoff].index
-        df.ix[zeroIndex, col] = 0
+    # for i,col in enumerate(inputDF.columns):
+    #
+    #     progressPercent = ((i+1)/len(inputDF.columns))*100
+    #
+    #     sys.stdout.write("Progeres: %d%%  %d Out of %d   \r" % (progressPercent, (i+1), len(inputDF.columns)))
+    #     sys.stdout.flush()
+    #
+    #
+    #     values = df[col].values.flatten().tolist()
+    #
+    #     values.sort(reverse=True)
+    #
+    #     up = values[int(np.floor(percent*len(values)))]
+    #
+    #     values = values[::-1]
+    #
+    #     down = values[int(np.floor(percent*len(values)))]
+    #
+    #     df[col] = df[col].apply(mapter2)
 
-        lst = lst[::-1]
-        downCutoff = lst[int(0.1*len(lst))]
-        index = inputDF[inputDF[col] < downCutoff].index
-        df.ix[index, col] = -1
+
+
+    # values = df.values.flatten().tolist()
+
+    # values.sort(reverse=True)
+
+    # up = values[int(np.floor(percent*len(values)))]
+
+    # values = values[::-1]
+
+    # down = values[int(np.floor(percent*len(values)))]
+
+    df = df.applymap(mapter)
+
+    # df = df.applymap(mapter2)
+
+
+    #
+    #     lst = []
+    #     lst = inputDF[col].values.tolist()
+    #     lst.sort()
+    #
+    #     lst = lst[::-1]
+    #     # upCutoff = lst[int(0.1*len(lst))]
+    #     upCutoff = lst[499]
+    #     index = inputDF[inputDF[col] > upCutoff].index
+    #     df.loc[index, col] = 1
+    #
+    #     zeroIndex = inputDF[inputDF[col] < upCutoff].index
+    #     df.loc[zeroIndex, col] = 0
+    #
+    #     lst = lst[::-1]
+    #     # downCutoff = lst[int(0.1*len(lst))]
+    #     downCutoff = lst[499]
+    #     index = inputDF[inputDF[col] < downCutoff].index
+    #     df.loc[index, col] = -1
 
 
     return(df)
@@ -180,16 +291,18 @@ def createDownGeneSetLib(inputDF, path, name, details=None):
 
         lst = index.values.tolist()
 
-        lst.insert(0, col)
-        if details:
-            lst.insert(1, details[i])
-        else:
-            lst.insert(1, 'NA')
-        lst = ['{0}\t'.format(elem) for elem in lst] # add tabs between terms in the lst
-        lst.insert(len(lst), '\n') # add a newline char at the end of each lst
+        if len(lst) > 5 and len(lst) <= 2000:
 
-        with open(path+filenameGMT, 'a') as the_file:
-            the_file.writelines(lst)
+            lst.insert(0, col)
+            if details:
+                lst.insert(1, details[i])
+            else:
+                lst.insert(1, 'NA')
+            lst = ['{0}\t'.format(elem) for elem in lst] # add tabs between terms in the lst
+            lst.insert(len(lst), '\n') # add a newline char at the end of each lst
+
+            with open(path+filenameGMT, 'a') as the_file:
+                the_file.writelines(lst)
 
 
 def createUpAttributeSetLib(inputDF, path, name):
@@ -212,14 +325,14 @@ def createUpAttributeSetLib(inputDF, path, name):
 
         lst = index.values.tolist()
 
-        if len(lst) > 5:
-            lst.insert(0, col)
-            lst.insert(1, 'NA')
-            lst = ['{0}\t'.format(elem) for elem in lst] # add tabs between terms in the lst
-            lst.insert(len(lst), '\n') # add a newline char at the end of each lst
 
-            with open(path+filenameGMT, 'a') as the_file:
-                the_file.writelines(lst)
+        lst.insert(0, col)
+        lst.insert(1, 'NA')
+        lst = ['{0}\t'.format(elem) for elem in lst] # add tabs between terms in the lst
+        lst.insert(len(lst), '\n') # add a newline char at the end of each lst
+
+        with open(path+filenameGMT, 'a') as the_file:
+            the_file.writelines(lst)
 
     inputDF = inputDF.T
 
@@ -256,6 +369,8 @@ def createSimilarityMatrix(inputDF, metric):
     similarity_matrix = dist.pdist(inputDF, metric)
     similarity_matrix = dist.squareform(similarity_matrix)
     similarity_df = pd.DataFrame(data=similarity_matrix[0:,0:], index=inputDF.index, columns=inputDF.index)
+    similarity_df.index.name = ''
+    similarity_df.columns.name = ''
     similarity_df = similarity_df.applymap(lambda x: 1-x)
     return(similarity_df)
 
@@ -275,21 +390,44 @@ def createGeneList(inputDf):
         sys.stdout.write("Progeres: %d%%  %d Out of %d   \r" % (progressPercent, (i+1), len(gene_list.index)))
         sys.stdout.flush()
 
-        if gene_list.ix[index, 'GeneSym'] in getGeneIDs.index:
-            gene_list.ix[index, 'GeneID'] = getGeneIDs.ix[gene_list.ix[index, 'GeneSym'], 'Entrez Gene ID(supplied by NCBI)']
+        if gene_list.loc[index, 'GeneSym'] in getGeneIDs.index:
+            gene_list.loc[index, 'GeneID'] = getGeneIDs.loc[gene_list.loc[index, 'GeneSym'], 'Entrez Gene ID(supplied by NCBI)']
 
 
     return(gene_list)
 
-def createAttributeList(inputDF):
+def createAttributeList(inputDF, metaData=pd.DataFrame()):
 
-    attribute_list = pd.DataFrame(columns=['Attributes'])
+    if not metaData.empty:
 
-    attribute_list['Attributes'] = inputDF.columns
+        cols = metaData.columns.tolist()
+
+        cols.insert(0, 'Attributes')
+
+        attribute_list = pd.DataFrame(columns=cols)
+
+        attribute_list['Attributes'] = inputDF.columns
+
+        attribute_list.set_index('Attributes', inplace=True)
+
+        for i,attribute in enumerate(attribute_list.index):
+
+            progressPercent = ((i+1)/len(attribute_list.index))*100
+
+            sys.stdout.write("Progeres: %d%%  %d Out of %d   \r" % (progressPercent, (i+1), len(attribute_list.index)))
+            sys.stdout.flush()
+
+            for col in attribute_list.columns:
+                attribute_list.loc[attribute, col] = metaData.loc[attribute, col]
+
+    else:
+        attribute_list = pd.DataFrame(columns=['Attributes'])
+        attribute_list['Attributes'] = inputDF.columns
+        attribute_list.set_index('Attributes', inplace=True)
 
     return(attribute_list)
 
-def createGeneAttributeEdgeList(inputDF, genelist, path, name):
+def createGeneAttributeEdgeList(inputDF, attributelist, genelist, path, name):
 
     count = 0
 
@@ -298,15 +436,22 @@ def createGeneAttributeEdgeList(inputDF, genelist, path, name):
     if os.path.isfile(path+filenameGMT):
         os.remove(path+filenameGMT)
 
-    lst = ['Attribute', 'Gene', 'GeneID', 'Weight']
-    lst = ['{0}\t'.format(elem) for elem in lst]
-    lst.insert(len(lst), '\n')
+
+    col = np.append(genelist.columns.values, attributelist.columns.values)
+    col = col.flatten().tolist()
+    col.insert(2, 'Attribute')
+    col.append('Weight')
+
+    temp = pd.DataFrame(columns=col)
+    # col = ['Attribute', 'Gene', 'GeneID', 'Weight']
+    col = ['{0}\t'.format(elem) for elem in col]
+
+    col.insert(len(col), '\n')
 
     with open(path+filenameGMT, 'a') as the_file:
-        the_file.writelines(lst)
+        the_file.writelines(col)
 
     # df = pd.DataFrame(columns=['Attribute', 'Gene', 'GeneID', 'Weight'])
-    temp = pd.DataFrame(columns=['Attribute', 'Gene', 'GeneID', 'Weight'])
 
     for i,col in enumerate(inputDF.columns):
 
@@ -315,18 +460,21 @@ def createGeneAttributeEdgeList(inputDF, genelist, path, name):
         sys.stdout.write("Progeres: %d%%  %d Out of %d   \r" % (progressPercent, (i+1), len(inputDF.columns)))
         sys.stdout.flush()
 
-        temp['Gene'] = inputDF[col].index
-        temp['Weight'] = inputDF[col].values.tolist()
-        temp['Attribute'] = [col]*len(temp['Gene'])
+        temp['GeneSym'] = inputDF[col].index
         temp['GeneID'] = genelist['GeneID']
+        temp['Attribute'] = [col]*len(temp['GeneSym'])
+        for col2 in attributelist.columns:
+            temp[col2] = [attributelist.loc[col, col2]]*len(temp['GeneSym'])
+        temp['Weight'] = inputDF[col].values.tolist()
 
         with open(path+filenameGMT, 'a') as the_file:
                  temp.to_csv(the_file, header=False, index=False, sep='\t')
 
-        count += temp[temp['Weight'] != 0].shape[0]
+        count += temp[temp['Weight'] >= 0.95].shape[0]
+        count += temp[temp['Weight'] <= -0.95].shape[0]
 
         # for index in temp.index:
-        #     lst = [temp.ix[index, 'Attribute'], temp.ix[index, 'Gene'], str(temp.ix[index, 'GeneID']), temp.ix[index, 'Weight']]
+        #     lst = [temp.loc[index, 'Attribute'], temp.loc[index, 'Gene'], str(temp.loc[index, 'GeneID']), temp.loc[index, 'Weight']]
         #     lst = ['{0}\t'.format(elem) for elem in lst]
         #     lst.insert(len(lst), '\n')
         #
@@ -335,7 +483,7 @@ def createGeneAttributeEdgeList(inputDF, genelist, path, name):
     print('\n\n The number of statisticaly relevent gene-attribute associations is: %d' %count)
 
 
-def createBinaryMatix(inputDF, ppi=False):
+def createBinaryMatrix(inputDF, ppi=False):
 
     if ppi:
 
@@ -356,7 +504,7 @@ def createBinaryMatix(inputDF, ppi=False):
             lst.discard(gene)
             lst = list(lst)
 
-            matrix.ix[gene, lst] = 1
+            matrloc.loc[gene, lst] = 1
 
         return(matrix)
 
@@ -379,6 +527,122 @@ def createBinaryMatix(inputDF, ppi=False):
             lst.discard(gene)
             lst = list(lst)
 
-            matrix.ix[gene, lst] = 1
+            matrix.loc[gene, lst] = 1
 
         return(matrix)
+
+def createStandardizedMatrix(inputDF):
+    df = inputDF.copy()
+
+    # def mapter2(x):
+    #     if x >= 0.95:
+    #         return x
+    #     elif x <= -0.95:
+    #         return x
+    #     else:
+    #         return 0
+
+    for i,index in enumerate(inputDF.index):
+
+        progressPercent = ((i+1)/len(inputDF.index))*100
+
+        sys.stdout.write("Progeres: %d%%  %d Out of %d   \r" % (progressPercent, (i+1), len(inputDF.index)))
+        sys.stdout.flush()
+
+        ourECDF = ECDF(df.loc[index])
+
+        df.loc[index] = ourECDF(df.loc[index])
+
+        mean = df.loc[index].values.flatten().mean()
+
+        df.loc[index] = df.loc[index].apply(lambda x: 2*(x-mean))
+
+        # df.loc[index] = df.loc[index].apply(mapter2)
+
+    values = df.values.flatten()
+    ourECDF = ECDF(values)
+
+    ourECDF = ourECDF(values).reshape((len(df.index), len(df.columns)))
+
+    newDF = pd.DataFrame(data = ourECDF, index=df.index, columns= df.columns)
+
+    newDF.index.name = ''
+
+    mean = newDF.values.flatten().mean()
+
+    newDF = newDF.applymap(lambda x: 2*(x-mean))
+
+    # for i,col in enumerate(inputDF.columns):
+    #
+    #     progressPercent = ((i+1)/len(inputDF.columns))*100
+    #
+    #     sys.stdout.write("Progeres: %d%%  %d Out of %d   \r" % (progressPercent, (i+1), len(inputDF.columns)))
+    #     sys.stdout.flush()
+    #
+    #     positiveAssociation = np.abs(df[df[col] > 0][col].values.tolist())
+    #     positiveAssociationIndex = df[df[col] > 0][col].index
+    #     positiveECDF = ECDF(positiveAssociation)
+    #
+    #     for j,value in enumerate(positiveAssociation):
+    #         df.loc[positiveAssociationIndex[j], col] = positiveECDF(value)
+    #
+    #     negativeAssociation = np.abs(df[df[col] < 0][col].values.tolist())
+    #     negativeAssociationIndex = df[df[col] < 0][col].index
+    #     negativeECDF = ECDF(negativeAssociation)
+    #
+    #     for k,value in enumerate(negativeAssociation):
+    #         df.loc[negativeAssociationIndex[k], col] = -negativeECDF(value)
+
+    return(newDF)
+
+def removeAndImpute(inputDF):
+
+    df = inputDF.copy()
+
+    #mean = np.mean(df.values.flatten())
+
+    df.reset_index(inplace=True)
+
+    df.replace(0.0, np.nan, inplace=True)
+
+    df.dropna(thresh=int(0.05*df.shape[0]), axis=1, inplace=True)
+    df.dropna(thresh=int(0.05*df.shape[1]), axis=0, inplace=True)
+
+    genes = df.iloc[:, 0].values.tolist()
+
+    df.drop(df.columns[0], axis=1, inplace=True)
+
+    #df.replace(np.nan, mean, inplace=True)
+
+    df = df.T.fillna(df.mean(axis=1)).T
+
+    df.index = genes
+
+    return(df)
+
+# def writeToZipFile(inputDF, path, name):
+#
+#     filename= name+'_%s'% str(datetime.date.today())[0:7].replace('-', '_')
+#
+#     if os.path.isfile(path+filename):
+#         os.remove(path+filename)
+#
+#
+#     for i, index in enumerate(inputDF.index):
+#
+#             progressPercent = ((i+1)/len(inputDF.columns))*100
+#
+#             sys.stdout.write("Progeres: %d%%  %d Out of %d   \r" % (progressPercent, (i+1), len(inputDF.columns)))
+#             sys.stdout.flush()
+#
+#
+#             lst = inputDF.iloc[i, :].values.tolist()
+#             lst.insert(0, index)
+#             lst = ['{0}\t'.format(elem) for elem in lst] # add tabs between terms in the lst
+#             lst.insert(len(lst), '\n') # add a newline char at the end of each lst
+#
+#             with open(path+filename, 'a') as the_file:
+#                 the_file.writelines(lst)
+#
+#     with ZipFile(path+filename+'.zip', 'w') as myzip:
+#         myzip.write(path+filename)
